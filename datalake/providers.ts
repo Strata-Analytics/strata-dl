92cee39c055c0225f6e283e3e34ee3e9bae12673
Luis Ramiro Krupoviesa <ramiro.krupoviesa@strata-analytics.us>
2026-04-03 10:09:03 -0300

    import deploy roles from requirements stack

--- datalake/providers.ts
+++ datalake/providers.ts
@@ -1,9 +1,16 @@
 import * as aws from "@pulumi/aws"
+import * as pulumi from "@pulumi/pulumi"
+
+const requirementsRef = new pulumi.StackReference(`organization/strata-dl-requirements/dev`)
+const devopsRoleArn = requirementsRef.getOutput('devopsRoleArn')
+const transformRoleArn = requirementsRef.getOutput('transformRoleArn')
+
+
 
 export const devopsProvider = new aws.Provider("devops-provider", {
   assumeRoles: [
     {
-      roleArn: ''
+      roleArn: devopsRoleArn
     },
   ],
 });
@@ -11,7 +18,7 @@ export const devopsProvider = new aws.Provider("devops-provider", {
 export const transformProvider = new aws.Provider("datalake-provider", {
   assumeRoles: [
     {
-      roleArn: ''
+      roleArn: transformRoleArn
     },
   ],
 });
