1a98122b23f49228a2dce68f4ff3342be3f76104
Luis Ramiro Krupoviesa <ramiro.krupoviesa@strata-analytics.us>
2026-04-04 12:18:25 -0300

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,17 @@ set -euo pipefail
 # DEPENDENCIES
 echo "==> 1/6 Checking for dependencies..."
 if ! command -v pulumi &>/dev/null; then
-  echo "==> Pulumi not found, installing..."
+  echo "==> pulumi not found, installing..."
   curl -fsSL https://get.pulumi.com | sh
 else
-  echo "Pulumi is installed. Continuing..."
+  echo "pulumi is installed. Continuing..."
 fi
 
 if ! command -v bun &>/dev/null; then
-  echo "==> Bun not found, installing..."
+  echo "==> bun not found, installing..."
   curl -fsSL https://bun.sh/install | bash
 else
-  echo "Bun is installed. Continuing..."
+  echo "bun is installed. Continuing..."
 fi
 
 # VARIABLES
@@ -30,6 +30,11 @@ export PULUMI_CONFIG_PASSPHRASE=''
 BUCKET_NAME=$(bun -e "import params from './params.json'; console.log(params.pulumiBackendBucketName);")
 GITHUB_REPO_ID=$(bun -e "import params from './params.json'; console.log(params.githubRepoId);")
 PROJECT_NAME=$(bun -e "import params from './params.json'; console.log(params.projectName);")
+PROJECT_NAME=$(bun -e "import params from './params.json'; console.log(params.projectName);")
+DEVOPS_DEV_PROFILE=$(bun -e "import params from './params.json'; console.log(params.profiles.dev.devops);")
+TRANSFORM_DEV_PROFILE=$(bun -e "import params from './params.json'; console.log(params.profiles.dev.transform);")
+DEVOPS_PROD_PROFILE=$(bun -e "import params from './params.json'; console.log(params.profiles.prod.devops);")
+TRANSFORM_PROD_PROFILE=$(bun -e "import params from './params.json'; console.log(params.profiles.prod.transform);")
 
 for var in BUCKET_NAME GITHUB_REPO_ID PROJECT_NAME; do
   if [[ -z "${!var}" ]]; then
